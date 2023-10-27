0c572f266c212759da2814f49835994cda97a246
abigail <abigail.arte@gmail.com>
2023-10-27 21:42:10 +0200

    player & crupier

--- BlackJack21/Program.cs
+++ BlackJack21/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BlackJack21.Crupier;
 
 namespace BlackJack21
 {
@@ -10,77 +11,101 @@ namespace BlackJack21
     {
         static void Main(string[] args)
         {
-            int objective = 21;
+            //    int objective = 21;
 
-            //initialize deck
-            Deck game = new Deck();
+            //    //initialize deck
+            //    Deck game = new Deck();
 
-            //number of cards:
-            Console.WriteLine("NUMBER OF CARDS: " + game.Count().ToString() );
+            //    //number of cards:
+            //    Console.WriteLine("NUMBER OF CARDS: " + game.Count().ToString() );
 
-            ////show cards: 
-            //Console.WriteLine("CARDS: "  );
-            //game.ShowDeck();
+            //    ////show cards: 
+            //    //Console.WriteLine("CARDS: "  );
+            //    //game.ShowDeck();
 
-            ////shuffle deck:
-            game.Shuffle();
-            //Console.WriteLine("CARDS AFTER SHUFFLE: ");
-            //game.ShowDeck();
+            //    ////shuffle deck:
+            //    game.Shuffle();
+            //    //Console.WriteLine("CARDS AFTER SHUFFLE: ");
+            //    //game.ShowDeck();
 
-            //steal a card
-            //Card stolenCard = game.Draw();
-            //Console.WriteLine("STOLEN CARD: " + stolenCard.GetInfo());
+            //    //steal a card
+            //    //Card stolenCard = game.Draw();
+            //    //Console.WriteLine("STOLEN CARD: " + stolenCard.GetInfo());
 
-            Card card1 = game.Draw();
-            Card card2 = game.Draw();
+            //    Card card1 = game.Draw();
+            //    Card card2 = game.Draw();
 
-            List<Card> cards = new List<Card>();
-            cards.Add(card1);
-            cards.Add(card2);
+            //    List<Card> cards = new List<Card>();
+            //    cards.Add(card1);
+            //    cards.Add(card2);
 
-            //INITIALIZE BLACKJACK PLAYER
-            BlackjackPlayer player = new BlackjackPlayer("Pepe", cards);
-            player.ShowHandInfo();
-
-            bool finish = false; 
-
-            while (!finish)
-            {
-                int points = player.GetPlayerPoints(); 
-
-                if (points > objective)
-                {
-                    Console.WriteLine("Has perdido, con " + points + " puntos");
-                    break; 
-                }
-                else if (points == objective)
-                {
-                    Console.WriteLine("Has ganado, con " + points + " puntos");
-                    break; 
-                }
-                else
-                {
-                    Console.WriteLine("Tienes " + points + " puntos");
-
-                    Console.WriteLine("Selecciona\n1.hit\n2.stand");
-                    string option = Console.ReadLine();
-
-                    if (option == "1")
-                    {
-                        Card newCard = game.Draw();
-                        player.AddCard(newCard);
-                        player.ShowHandInfo();
-                    }
-                    else if (option == "2")
-                    {
-                        Console.WriteLine("Bye bye!");
-                        finish = true; 
-                    }
-                }
-            }
+            //    //INITIALIZE BLACKJACK PLAYER
+            //    BlackjackPlayer player = new BlackjackPlayer("Pepe", cards);
+            //    player.ShowHandInfo();
+            //    player.DrawCards();
+            //    Crupier crupier = new Crupier("Crupier", new List<Card>());
+            //    crupier.Play(game);
 
 
-            string input = Console.ReadLine();
+            //    bool finish = false; 
+
+            //    while (!finish)
+            //    {
+            //        int points = player.GetPlayerPoints(); 
+
+            //        if (points > objective)
+            //        {
+            //            Console.WriteLine("Has perdido, con " + points + " puntos");
+            //            break; 
+            //        }
+            //        else if (points == objective)
+            //        {
+            //            Console.WriteLine("Has ganado, con " + points + " puntos");
+            //            break; 
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Tienes " + points + " puntos");
+
+            //            Console.WriteLine("Selecciona\n1.hit\n2.stand");
+            //            string option = Console.ReadLine();
+
+            //            if (option == "1")
+            //            {
+            //                Card newCard = game.Draw();
+            //                player.AddCard(newCard);
+            //                player.ShowHandInfo();
+            //                player.DrawCards();
+            //            }
+            //            else if (option == "2")
+            //            {
+            //                Console.WriteLine("Bye bye!");
+            //                finish = true; 
+            //            }
+            //        }
+            //    }
+
+
+            //    string input = Console.ReadLine();
+            //}
+            Deck deck = new Deck();
+            // Crear una instancia del jugador
+            List<Card> playerHand = new List<Card>();
+            BlackjackPlayer player = new BlackjackPlayer("Pepe", playerHand);
+
+            // Crear una instancia del crupier
+            List<Card> crupierHand = new List<Card>();
+            Crupier crupier = new Crupier("Crupier", crupierHand);
+
+            // Crear una instancia del juego de Blackjack
+            BlackjackGame game = new BlackjackGame(player, crupier);
+
+            // Iniciar el juego llamando al método PlayGame
+            game.PlayGame(deck);
+
+            Console.WriteLine("Juego terminado. Presiona Enter para salir.");
+            Console.ReadLine();
+
         }
     }
 
@@ -180,11 +205,6 @@ namespace BlackJack21
                 }
             }
 
-            //foreach (Card card in cards)
-            //{
-            //    Console.WriteLine(card.GetInfo());
-            //}
-
         }
 
     }
@@ -214,6 +234,29 @@ namespace BlackJack21
             }
         }
 
+        public void DrawCards()
+        {
+            string[] lines = new string[5];
+
+            foreach (Card card in hand)
+            {
+                string cardValue = card.GetValue().ToString();
+                char cardSuit = card.GetSuit();
+
+                lines[0] += " +-----+";
+                lines[1] += " |" + cardValue.PadLeft(2) + "   |";
+                lines[2] += " |  " + cardSuit + "  |";
+                lines[3] += " |   " + cardValue.PadRight(2) + "|";
+                lines[4] += " +-----+";
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+        }
+
+
     }
 
     //BLACKJACK PLAYER
@@ -236,7 +279,7 @@ namespace BlackJack21
             foreach (var card in hand)
             {
                 int cardValue = card.GetValue();
-                if (cardValue == 1)
+                if (cardValue == 1 && (points +11) <=21)
                 {
                     points += 11;
                 }
@@ -252,15 +295,154 @@ namespace BlackJack21
         {
             return GetPlayerPoints() > 21;
         }
+
     }
 
     //CRUPIER
     public class Crupier : BlackjackPlayer
     {
-       
         public Crupier(string name, List<Card> hand) : base(name, hand)
         {
+        }
+        public void DrawInitialCards()
+        {
+            Card firstCard = hand[0];
+            Card secondCard = hand[1];
 
+            string cardValue1 = firstCard.GetValue().ToString();
+            char cardSuit1 = firstCard.GetSuit();
+
+            string[] lines = new string[5];
+
+            // Mostrar ambas cartas del crupier una al lado de la otra
+            lines[0] = " +-----+ +-----+  ";
+            lines[1] = " |" + cardValue1.PadLeft(2) + "   | |     |  ";
+            lines[2] = " |  " + cardSuit1 + "  | |     |  ";
+            lines[3] = " |   " + cardValue1.PadRight(2) + "| |     |  ";
+            lines[4] = " +-----+ +-----+  ";
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+        }
+
+
+
+        public void Play(Deck deck)
+        {
+            while (GetPlayerPoints() < 17)
+            {
+                Card newCard = deck.Draw();
+                AddCard(newCard);
+                Console.WriteLine("El crupier toma una carta: " + newCard.GetInfo());
+            }
+        }
+    }
+
+
+    public class BlackjackGame
+    {
+        private BlackjackPlayer player;
+        private Crupier crupier;
+
+        public BlackjackGame(BlackjackPlayer player, Crupier crupier)
+        {
+            this.player = player;
+            this.crupier = crupier;
+        }
+
+        public void PlayGame(Deck deck)
+        {
+            Console.WriteLine("¡Comienza el juego de Blackjack!");
+
+         
+            deck.Shuffle();
+
+           
+            player.AddCard(deck.Draw());
+            player.AddCard(deck.Draw());
+            crupier.AddCard(deck.Draw());
+            crupier.AddCard(deck.Draw());
+
+            Console.WriteLine("MANO DEL CRUPIER:");
+            crupier.DrawInitialCards(); 
+
+            Console.WriteLine("TU MANO:");
+            player.DrawCards();
+
+            bool finish = false;
+            int playerPoints = player.GetPlayerPoints();
+            int crupierPoints = crupier.GetPlayerPoints();
+            Console.WriteLine("PLAYER: " + playerPoints);
+            Console.WriteLine("CRUPIER: " + crupierPoints);
+
+            if (player.GetPlayerPoints() == 21 && crupierPoints != 21)
+            {
+                Console.WriteLine("HAS GANADO");
+                Console.WriteLine("PLAYER: " + playerPoints);
+                Console.WriteLine("CRUPIER: " + crupierPoints);
+                finish = true;
+            }
+
+         
+
+           
+
+            while (!finish)
+            {
+              
+                break;
+                //if (points > 21)
+                //{
+                //    Console.WriteLine("Has perdido, con " + points + " puntos");
+                //    return;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Tienes " + points + " puntos");
+
+                //    Console.WriteLine("Selecciona\n1. Pedir carta\n2. Plantarse");
+                //    string option = Console.ReadLine();
+
+                //    if (option == "1")
+                //    {
+                //        Card newCard = deck.Draw();
+                //        player.AddCard(newCard);
+                //        player.DrawCards();
+                //    }
+                //    else if (option == "2")
+                //    {
+                //        break;
+                //    }
+                //}
+            }
+
+            // El crupier juega
+            //crupier.Play(deck);
+
+            //// Determinar el resultado del juego
+            //DetermineWinner();
+          
+        }
+
+        private void DetermineWinner()
+        {
+            int playerPoints = player.GetPlayerPoints();
+            int crupierPoints = crupier.GetPlayerPoints();
+
+            if (playerPoints > 21 || (crupierPoints <= 21 && crupierPoints >= playerPoints))
+            {
+                Console.WriteLine("¡Gana el Crupier!");
+            }
+            else if (crupierPoints > 21 || playerPoints > crupierPoints)
+            {
+                Console.WriteLine("¡Gana el Jugador!");
+            }
+            else
+            {
+                Console.WriteLine("¡Empate!");
+            }
         }
     }
 
